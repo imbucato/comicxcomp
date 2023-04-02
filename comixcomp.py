c3d3f26ae8df65de17e295b7df97f952773317a2
imbucato <45110108+imbucato@users.noreply.github.com>
2023-04-02 17:57:17 +0200

    integrato il multiprocessing g

--- comixcomp.py
+++ comixcomp.py
@@ -1,4 +1,5 @@
 import os
+import time
 import sys
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
@@ -7,6 +8,7 @@ import rarfile
 import zipfile
 import tempfile
 import shutil
+from multiprocessing import Pool
 
 #Definisco il percorso dell'eseguibile unrar.exe indispensabile per il funziomanento del modulo python 'rarfile'
 rarfile.UNRAR_TOOL = r'UnRAR.exe'
@@ -27,8 +29,49 @@ def is_rar_file(filename):
     except rarfile.NotRarFile:
         return False
 
+#FUNZIONE PER LA CONVERSIONE DELLE IMMAGINI: VIENE RICHIAMATA ALL'INTERNO DEL POOL MUTLIPROCESSING
+def elabora_immagine(max_size, dpi, jpg_quality, color_bits,new_temp_dir, pagine, pagina, file):
+    
+    nome_immagine_senza_estensione = os.path.splitext(os.path.basename(file))[0]
+    pagina = pagina + 1
+    
+    output_image_path_jpg = os.path.join(new_temp_dir, nome_immagine_senza_estensione + '.jpg')
+    output_image_path_png = os.path.join(new_temp_dir, nome_immagine_senza_estensione + '.png')
+
+    img = Image.open(file)
+
+    #Se la dimensione del lato lungo inserita dall'utente è minore della dimensione originale l'immagine viene scalata                
+    if max(img.size) > max_size:
+        scale_factor = max_size / max(img.size)
+        new_size = tuple(int(x * scale_factor) for x in img.size)
+        img = img.resize(new_size, Image.LANCZOS)
+
+    #if img.info['dpi'] < dpi: dpi=img.info['dpi']
+
+    #Determinazione del compression level per il salvataggio in PNG
+    comp_level_png = int((100-jpg_quality)/10)
+    if comp_level_png == 10: comp_level_png=9
+
+    #Se è stata selezionata la modalità "bianco e nero" vengono lasciate a colori solo prima, seconda, terza e quarta di copertina
+    #Le altre immagini vengono convertite in scala di grigi ad 8 tonalità
+    if color_bits=='B/N' and 2 < pagina < pagine - 1 and img.mode != '1':
+        if img.mode != 'P' or img.getcolors(maxcolors=256) is None:
+            img = img.convert('L')
+            img = img.quantize(8)
+
+    #Se l'immagine è in b/n, scala di grigi o modalità Palette viene salvata in PNG               
+    if img.mode == '1' or img.mode == 'P' or img.mode == 'L':
+        img.save(output_image_path_png, format="PNG", dpi=(dpi, dpi), compress_level=comp_level_png) 
+    #Altrimenti viene convertita in RBA (nel caso in cui sia in RGBA) e salvata in JPEG
+    else:
+        if img.mode == "RGBA" : 
+            img=img.convert("RGB")
+        img.save(output_image_path_jpg, 'JPEG', dpi=(dpi, dpi), quality=jpg_quality)    
+
+
 #FUNZIONE PRINCIPALE PER LA COMPRESSIONE DEI CBR/CBZ
 def compress_cb(input_file, output_file, max_size, dpi, jpg_quality, color_bits):
+   
     print_status('Inizia compressione')
 
     archive = None
@@ -57,50 +100,28 @@ def compress_cb(input_file, output_file, max_size, dpi, jpg_quality, color_bits)
         temp_dir = tempfile.mkdtemp()
         print_status('Scompattazione archivio')
         af.extractall(temp_dir)
-        pagina = 1
+       
         new_temp_dir = tempfile.mkdtemp()
+        
+        lista_file = []
+        
         for root, _, files in os.walk(temp_dir):
             print_status('Modifica immagini')
             for file in files:
                 if file.lower().endswith(('.jpg', '.jpeg', '.png')):
                     image_path = os.path.join(root, file)
-
-                    nome_immagine_senza_estensione = os.path.splitext(os.path.basename(image_path))[0]
-
-                    output_image_path_jpg = os.path.join(new_temp_dir, nome_immagine_senza_estensione + '.jpg')
-                    output_image_path_png = os.path.join(new_temp_dir, nome_immagine_senza_estensione + '.png')
-
-                    img = Image.open(image_path)
-
-                    if max(img.size) > max_size:
-                        scale_factor = max_size / max(img.size)
-                        new_size = tuple(int(x * scale_factor) for x in img.size)
-                        img = img.resize(new_size, Image.LANCZOS)
-
-                    if color_bits=='grigi' and 2 < pagina < len(files) - 1 and img.mode != '1':
-                        img = img.convert("L");
-                    elif color_bits=='bn' and 2 < pagina < len(files) - 1 and img.mode != '1':
-                        img = img.convert("L");
-                        img = img.convert("1");
-                    
-                    if img.mode == '1' or img.mode == 'P':
-                        img.save(output_image_path_png, format="PNG", dpi=(dpi, dpi), compress_level=6) 
-                    else:
-                        if img.mode == "RGBA" : img=img.convert("RGB")
-                        img.save(output_image_path_jpg, 'JPEG', dpi=(dpi, dpi), quality=jpg_quality)          
-                    
-                    pagina += 1
-       
-        print_status('Ricreazione archivio compresso')
-        
+                    lista_file.append(image_path)
+        #Avvio del multiprocessing per la conversione delle immagini           
+        with Pool() as pool:
+            pool.starmap(elabora_immagine, [(max_size, dpi, jpg_quality, color_bits, new_temp_dir, len(lista_file)+1, i, file,) for i, file in enumerate(lista_file)])            
+                  
         with zipfile.ZipFile(output_file, 'w', zipfile.ZIP_DEFLATED) as out_zip:
             for root, _, files in os.walk(new_temp_dir):
                 for file in files:
                     out_zip.write(os.path.join(root, file), arcname=file)
-              
+             
         shutil.rmtree(temp_dir)
         shutil.rmtree(new_temp_dir)
-
         print_status('Fine compressione')
 
 # Definisce una funzione per la selezione del file di input
@@ -128,7 +149,7 @@ def print_status(message):
 
 # Definisce la funzione che avvia la compressione
 def avvia_compressione():
-    
+    inizio = time.time()
     avvia = True
 
     input_file = input_file_entry.get()
@@ -169,123 +190,113 @@ def avvia_compressione():
     #except:
         #print_status("SI E' VERIFICATO UN ERRORE DURANTE LA CONVERSIONE")
 
-#root.title("undefined")
-width=592
-height=424
+    fine=time.time()
+    durata = int(fine-inizio)
+    print_status(f'L\'operazione è durata {durata} secondi')
 
-#setting window size
-window = tk.Tk()
-window.title("Comrpimi cbr/cbz singolo")
-window.geometry('%dx%d' % (width, height))
-window.resizable(width=False, height=False)
-screenwidth = window.winfo_screenwidth()
-screenheight = window.winfo_screenheight()
+########PARTE PRICIPALE DEL PROGRAMMA CON DEFINIZIONE DELLA FINESTRA
+#(Processo principale all'interno del quale avviene il multiprocessing)
 
-alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-window.geometry(alignstr)
+if __name__ == '__main__':
 
-# Crea i widget per la selezione del file di input
-input_file_label = tk.Label(window, text="Seleziona il file cbr/cbz da comprimere")
-input_file_label.place(x=10,y=0,height=30)
-input_file_entry = tk.Entry(window)
-input_file_entry.configure(state='disabled')
-input_file_entry.place(x=10,y=30,width=486,height=30)
-input_file_button = tk.Button(window, text="Sfoglia", command=select_input_file)
-input_file_button.place(x=510,y=30,width=70,height=30)
+    #Dimensioni della finestra
+    width=592
+    height=424
 
-# Crea i widget per la selezione del file di output
-output_file_label = tk.Label(window, text="Scegli il nome del file cbz compresso")
-output_file_label.place(x=10,y=70,height=30)
-output_file_entry = tk.Entry(window)
-output_file_entry.configure(state='disabled')
-output_file_entry.place(x=10,y=100,width=487,height=30)
-output_file_button = tk.Button(window, text="Sfoglia", command=select_output_file)
-output_file_button.place(x=510,y=100,width=70,height=30)
+    #setting window size
+    window = tk.Tk()
+    window.iconbitmap('ico.ico')
+    window.title("Comrpimi un file cbr/cbz")
+    window.geometry('%dx%d' % (width, height))
+    window.resizable(width=False, height=False)
+    screenwidth = window.winfo_screenwidth()
+    screenheight = window.winfo_screenheight()
 
-#Funzione di validazione del campo "long_side_entry": impedisce che vengano inserite lettere
-def validate_input(new_value):
-    if new_value.isnumeric() or new_value == "":
-        return True
-    else:
-        return False
+    alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+    window.geometry(alignstr)
 
-vcmd = (window.register(validate_input), '%P')
+    # Crea i widget per la selezione del file di input
+    input_file_label = tk.Label(window, text="Seleziona il file cbr/cbz da comprimere")
+    input_file_label.place(x=10,y=0,height=30)
+    input_file_entry = tk.Entry(window)
+    input_file_entry.configure(state='disabled')
+    input_file_entry.place(x=10,y=30,width=486,height=30)
+    input_file_button = tk.Button(window, text="Sfoglia", command=select_input_file)
+    input_file_button.place(x=510,y=30,width=70,height=30)
 
-# Crea i widget per l'inserimento dei parametri di compressione
-long_side_label = tk.Label(window, text="Dimensione lato lungo")
-long_side_label.place(x=0,y=140,width=151,height=30)
-long_side_entry = tk.Entry(window,validate="key", validatecommand=vcmd)
-long_side_entry["justify"] = "center"
-long_side_entry.place(x=30,y=170,width=92,height=30)
+    # Crea i widget per la selezione del file di output
+    output_file_label = tk.Label(window, text="Scegli il nome del file cbz compresso")
+    output_file_label.place(x=10,y=70,height=30)
+    output_file_entry = tk.Entry(window)
+    output_file_entry.configure(state='disabled')
+    output_file_entry.place(x=10,y=100,width=487,height=30)
+    output_file_button = tk.Button(window, text="Sfoglia", command=select_output_file)
+    output_file_button.place(x=510,y=100,width=70,height=30)
 
-dpi_label = tk.Label(window, text="DPI")
-dpi_label.place(x=170,y=140,width=70,height=30)
+    #Funzione di validazione del campo "long_side_entry": impedisce che vengano inserite lettere
+    def validate_input(new_value):
+        if new_value.isnumeric() or new_value == "":
+            return True
+        else:
+            return False
 
-#Variabile per memorizzare il valore selezionato dall'elenco a discesa
-selected_value = tk.StringVar()
-options = [72, 96, 120, 150, 200, 220, 300, 400, 600]
-selected_value.set(options[0])
-dpi_entry = ttk.Combobox(window,textvariable=selected_value, values=options)
-dpi_entry["justify"] = "center"
-dpi_entry.place(x=170,y=170,width=70,height=30)
+    vcmd = (window.register(validate_input), '%P')
 
-jpg_comp_label = tk.Label(window, text="Compressione JPEG")
-jpg_comp_label.place(x=250,y=140,width=140,height=30)
+    # Crea i widget per l'inserimento dei parametri di compressione
+    long_side_label = tk.Label(window, text="Dimensione lato lungo")
+    long_side_label.place(x=0,y=140,width=151,height=30)
+    long_side_entry = tk.Entry(window,validate="key", validatecommand=vcmd)
+    long_side_entry["justify"] = "center"
+    long_side_entry.place(x=30,y=170,width=92,height=30)
 
-compressione_jpg = tk.IntVar(value=85)
-jpg_comp_entry = tk.Scale(window, from_=1, to=100, orient=tk.HORIZONTAL, variable=compressione_jpg)
-jpg_comp_entry.place(x=260,y=160,width=120)
+    dpi_label = tk.Label(window, text="DPI")
+    dpi_label.place(x=170,y=140,width=70,height=30)
 
-#jpg_comp_entry = tk.Entry(window)
-#jpg_comp_entry["justify"] = "center"
-#jpg_comp_entry.place(x=280,y=170,width=70,height=30)
+    #Variabile per memorizzare il valore selezionato dall'elenco a discesa
+    selected_value = tk.StringVar()
+    options = [72, 96, 120, 150, 200, 220, 300, 400, 600]
+    selected_value.set(options[0])
+    dpi_entry = ttk.Combobox(window,textvariable=selected_value, values=options)
+    dpi_entry["justify"] = "center"
+    dpi_entry.place(x=170,y=170,width=70,height=30)
 
-# Crea i widget per la selezione delle radiobox colore/scala di grigi/bw
-GLabel_183=tk.Label(window)
-GLabel_183["justify"] = "center"
-GLabel_183["text"] = "Colore"
-GLabel_183.place(x=390,y=140,width=70,height=30)
+    jpg_comp_label = tk.Label(window, text="Qualità")
+    jpg_comp_label.place(x=250,y=140,width=140,height=30)
 
-GLabel_58=tk.Label(window)
-GLabel_58["justify"] = "center"
-GLabel_58["text"] = "Scala grigi"
-GLabel_58.place(x=460,y=140,width=70,height=30)
+    compressione_jpg = tk.IntVar(value=85)
+    jpg_comp_entry = tk.Scale(window, from_=1, to=100, orient=tk.HORIZONTAL, variable=compressione_jpg)
+    jpg_comp_entry.place(x=260,y=160,width=120)
 
-GLabel_417=tk.Label(window)
-GLabel_417["justify"] = "center"
-GLabel_417["text"] = "B/W"
-GLabel_417.place(x=530,y=140,width=70,height=30)
+    # Crea i widget per la selezione della tipologia di fumetto in entrata
+    GLabel_58=tk.Label(window)
+    GLabel_58["justify"] = "center"
+    GLabel_58["text"] = "Tipologia fumetto in entrata"
+    GLabel_58.place(x=400,y=140,width=160,height=30)
 
-radio_var = tk.StringVar()
-radio_colori = tk.Radiobutton(window, variable=radio_var, value='colori')
-radio_colori.place(x=380,y=170,width=85,height=25)
+    radio_var = tk.StringVar()
+    radio_colori = tk.Radiobutton(window, variable=radio_var, value='colori', text='Colori')
+    radio_colori.place(x=390,y=170,width=100,height=25)
 
-radio_grigi = tk.Radiobutton(window, variable=radio_var, value='grigi')
-radio_grigi.place(x=450,y=170,width=85,height=25)
+    radio_grigi = tk.Radiobutton(window, variable=radio_var, value='B/N', text='B/N')
+    radio_grigi.place(x=480,y=170,width=85,height=25)
 
-radio_bn = tk.Radiobutton(window, variable=radio_var, value='bn')
-radio_bn.place(x=520,y=170,width=85,height=25)
+    radio_var.set('colori')     #imposto colori come valore di default
 
-radio_var.set('colori')     #imposto colori come valore di default
+    # Crea il widget per il pulsante di conferma
+    confirm_button = tk.Button(window, text="AVVIA", command=avvia_compressione)
+    confirm_button.place(x=250,y=220,width=110,height=30)
 
-# Crea il widget per il pulsante di conferma
-#confirm_button = tk.Button(window, text="AVVIA", command=window.quit)
-confirm_button = tk.Button(window, text="AVVIA", command=avvia_compressione)
-confirm_button.place(x=250,y=220,width=110,height=30)
+    # Crea il widget text per la stampa dei messaggi di stato
+    output_message = tk.Text(window)
 
-# Crea il widget text per la stampa dei messaggi di stato
-output_message = tk.Text(window)
+    scrollbar = tk.Scrollbar(window)
+    scrollbar.config(command=output_message.yview)
 
-scrollbar = tk.Scrollbar(window)
-scrollbar.config(command=output_message.yview)
+    output_message.config(yscrollcommand=scrollbar.set)
 
-output_message.config(yscrollcommand=scrollbar.set)
+    output_message.place(x=30,y=270,width=530,height=131)
+    scrollbar.place(x=550,y=270,width=20,height=131)
+    output_message.configure(state='disabled')
 
-output_message.place(x=30,y=270,width=530,height=131)
-scrollbar.place(x=550,y=270,width=20,height=131)
-output_message.configure(state='disabled')
-
-
-
-# Avvia il loop principale della GUI
-window.mainloop()
+    # Avvia il loop principale della GUI
+    window.mainloop()

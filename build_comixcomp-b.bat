4aae1baa976553b4f1ca3b48b114fe5a669b0d9c
imbucato <roby1976@gmail.com>
2023-04-12 16:17:36 +0200

    modificto batfile

--- build_comixcomp-b.bat
+++ build_comixcomp-b.bat
@@ -5,3 +5,7 @@ pyinstaller --distpath ./build --onefile --icon=ico.ico --add-data "ico.ico;." -
 pyinstaller --distpath ./build --onefile --icon=ico-batch.ico --add-data "UnRAR.exe" --noconsole .\comixcomp-b.py
 
 pyinstaller --distpath ./build --onefile --icon=ico-batch.ico --add-data "UnRAR.exe" --noconsole .\comixcomp.py
+
+pyinstaller --distpath ./build --onefile --icon=ico-batch.ico --noconsole .\comixcomp-b.py
+
+pyinstaller --distpath ./build --onefile --icon=ico.ico --noconsole .\comixcomp.py
